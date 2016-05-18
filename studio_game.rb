b6ef008ba12a5523f7dacc9b77403ab082c98280
Tijo <emailtijothomas@gmail.com>
2016-05-18 18:04:31 -0500

    Added Player class. Added initialize and to_s methods to initialize state and print the current state of the object.

--- studio_game.rb
+++ studio_game.rb
@@ -1,13 +1,38 @@
-def time
-	current_time = Time.new
-	current_time.strftime("%I:%M:%S")
+class Player
+	
+	def initialize(name, health=100)
+		@name = name.capitalize
+		@health = health
+	end
+
+	def to_s
+		"I'm #{@name} with a health of #{@health}."
+	end
+
+	def blam
+		@health -= 10
+		puts "#{@name} got blammed!"
+	end
+
+	def woot
+		@health += 15
+		puts "#{@name} got wooted!"
+	end
 end
 
-def say_hello(name, health=100)
-	"I'm #{name.capitalize} with a health of #{health} as of #{time}."
-end
+	player1 = Player.new("moe")
+	puts player1
+	player1.blam
+	puts player1
 
-puts say_hello("larry", 60)
-puts say_hello("curly", 125)
-puts say_hello("moe")
-puts say_hello("shemp", 90)
+	player2 = Player.new("larry", 60)
+	puts player2
+	player2.woot
+	puts player2
+
+	player3 = Player.new("curly", 125)
+	puts player3
+	player3.blam
+	puts player3
+	player3.woot
+	puts player3
