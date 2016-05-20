0a33f7a6dd699f6fcdef980a193e2f5dd92e4697
Tijo <emailtijothomas@gmail.com>
2016-05-20 16:18:11 -0500

    Added a 'Game' class that executes the scoring methods from the 'Player' class.

--- studio_game.rb
+++ studio_game.rb
@@ -26,8 +26,32 @@ class Player
 		@health + @name.length
 	end
 
-	def name=(new_name)
-		@name = new_name.capitalize
+end
+
+class Game
+	
+	attr_reader :title
+
+	def initialize(title)
+		@title = title
+		@players = []
+	end
+
+	def add_player(a_player)
+		@players << a_player
+	end
+
+	def play
+		puts "There are #{@players.size} players in #{@title}:"
+		@players.each do |player|
+			puts player
+		end
+		@players.each do |player|
+			player.blam
+			player.woot
+			player.woot
+			puts player
+		end
 	end
 
 end
@@ -36,24 +60,9 @@ player1 = Player.new("moe")
 player2 = Player.new("larry", 60)
 player3 = Player.new("curly", 125)
 
-players = [player1, player2, player3]
-
-puts "There are #{players.size} players in the game:"
-players.each do |player|
-	puts player
-end
-
-players.each do |player|
-	player.health
-end
-
-players.each do |player|
-	player.woot
-	player.blam
-	puts player
-end
-
-players.pop
-player4 = Player.new("Shemp", 90)
-players << player4
-puts players
+knuckleheads = Game.new("Knuckleheads")
+puts knuckleheads.title
+knuckleheads.add_player(player1)
+knuckleheads.add_player(player2)
+knuckleheads.add_player(player3)
+knuckleheads.play
