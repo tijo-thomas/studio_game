1c4e270790ca24d8969174b8e5e2cb8ec2b51492
Tijo <emailtijothomas@gmail.com>
2016-05-20 12:18:37 -0500

    Added attributes, as well as 'attr' methods. Created 'virtual attribute' (score method) by using existing attributes and using them in calculation.

--- studio_game.rb
+++ studio_game.rb
@@ -1,12 +1,15 @@
 class Player
 	
+	attr_reader :health
+	attr_accessor :name
+
 	def initialize(name, health=100)
 		@name = name.capitalize
 		@health = health
 	end
 
 	def to_s
-		"I'm #{@name} with a health of #{@health}."
+		"I'm #{@name} with a health of #{@health} and a score of #{score}."
 	end
 
 	def blam
@@ -18,17 +21,29 @@ class Player
 		@health += 15
 		puts "#{@name} got wooted!"
 	end
+
+	def score
+		@health + @name.length
+	end
+
+	def name=(new_name)
+		@name = new_name.capitalize
+	end
 end
 
 	player1 = Player.new("moe")
 	puts player1
 	player1.blam
 	puts player1
+	puts player1.health
 
 	player2 = Player.new("larry", 60)
 	puts player2
 	player2.woot
 	puts player2
+	puts player2.health
+	player2.name = "lawrence"
+	puts player2.name
 
 	player3 = Player.new("curly", 125)
 	puts player3
@@ -36,3 +51,4 @@ end
 	puts player3
 	player3.woot
 	puts player3
+	puts player3.health
