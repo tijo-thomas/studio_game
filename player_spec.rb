5156470a23343a716f4c6778d729f8fbb230af7a
Tijo <emailtijothomas@gmail.com>
2016-05-21 16:00:45 -0500

    Added RSpec and a spec file to test the Player class.

---
+++ player_spec.rb
@@ -0,0 +1,38 @@
+require_relative "player"
+
+describe Player do
+
+	before do
+		@initial_health = 150
+		@player = Player.new("larry", @initial_health)
+	end
+	
+	it "has a capitalized name" do
+		@player.name.should == "Larry"
+	end
+
+	it "has an initial health" do
+		@player.health.should == 150
+	end
+
+	it "has a string representation" do
+		@player.to_s.should == "I'm Larry with a health of 150 and a score of 155."
+	end
+
+	it "computes a score as the sum of its health and length of name" do
+		@player.score.should == 155
+	end
+
+	it "increases health by 15 when wooted" do
+		@player.woot
+
+		@player.health.should == @initial_health + 15
+	end
+
+	it "decreases health by 10 when blammed" do
+		@player.blam
+
+		@player.health.should == @initial_health - 10
+	end
+
+end
