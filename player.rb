9d8d3697833ecdc3aea9698039e9b44583b02c1a
Tijo <emailtijothomas@gmail.com>
2016-05-21 00:02:37 -0500

    Put classes in their own separate files. Left driver code in original file. Setup 'require_relative' statements.

---
+++ player.rb
@@ -0,0 +1,42 @@
+class Player
+	
+	attr_accessor :name
+	attr_reader :health
+
+	def initialize(name, health=100)
+		@name = name.capitalize
+		@health = health
+	end
+
+	def to_s
+		"I'm #{@name} with a health of #{@health} and a score of #{score}."
+	end
+
+	def blam
+		@health -= 10
+		puts "#{@name} got blammed!"
+	end
+
+	def woot
+		@health += 15
+		puts "#{@name} got wooted!"
+	end
+
+	def score
+		@health + @name.length
+	end
+
+end
+
+# The if expression only runs the enclosing code if the name
+# of the current source file (__FILE__) is the same as the name
+# of the Ruby program file being run
+if __FILE__ == $0
+	player = Player.new("moe")
+	puts player.name
+	puts player.health
+	player.woot
+	puts player.health
+	player.blam
+	puts player.health
+end
