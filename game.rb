8098f894e3d9d6cc3736907a0968e04bfdd43951
Tijo <emailtijothomas@gmail.com>
2016-05-24 20:31:18 -0500

    Added a 'print_stats' method to the Game class. The 'print_stat's method splits players into 2 arrays using partition method. The players and their health are printed for each array using and each method. The 'print_stats' method is called in the studio_game.rb file.

--- game.rb
+++ game.rb
@@ -14,6 +14,22 @@ class Game
 		@players << a_player
 	end
 
+	def print_stats
+		strong_players, whimpy_players = @players.partition { |player| player.strong? }
+
+		puts "\n#{@title} Statistics:"
+
+		puts "\n#{strong_players.size} strong players"
+		strong_players.each do |player|
+			puts "#{player.name} (#{player.health})"
+		end
+
+		puts "\n#{whimpy_players.size} whimpy players"
+		whimpy_players.each do |player|
+			puts "#{player.name} (#{player.health})"
+		end
+	end
+
 	def play(rounds)
 		puts "There are #{@players.size} players in #{@title}:"
 		
