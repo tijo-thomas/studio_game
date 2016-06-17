592a3755ab704e4f3ed63b576b6f49b6a1d7d29d
Tijo <emailtijothomas@gmail.com>
2016-06-16 23:46:52 -0500

    Prompts the user for imput.

--- studio_game.rb
+++ studio_game.rb
@@ -7,8 +7,14 @@ player3 = Player.new("curly", 125)
 
 knuckleheads = Game.new("Knuckleheads")
 puts knuckleheads.title
-knuckleheads.add_player(player1)
-knuckleheads.add_player(player2)
-knuckleheads.add_player(player3)
+
+# knuckleheads.add_player(player1)
+# knuckleheads.add_player(player2)
+# knuckleheads.add_player(player3)
+
+loop do
+	puts "\nHow many games rounds? ('quit' to exit)"
+end
+
 knuckleheads.play(3)
 knuckleheads.print_stats
