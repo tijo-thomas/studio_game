d62231d8c322fb5402874ded5b03040e8cec8fba
Tijo <emailtijothomas@gmail.com>
2016-06-18 15:47:37 -0500

    Creates a new 'ClumsyPlayer' that inherits from the 'Player' class.

--- studio_game.rb
+++ studio_game.rb
@@ -1,5 +1,6 @@
 require_relative "player"
 require_relative "game"
+require_relative "clumsy_player"
 
 player1 = Player.new("moe")
 player2 = Player.new("larry", 60)
@@ -7,6 +8,10 @@ player3 = Player.new("curly", 125)
 
 knuckleheads = Game.new("Knuckleheads")
 knuckleheads.load_players(ARGV.shift || "players.csv") # Calls the load_players method and passes the csv file entered as command-line argument or defaults to "players.csv".
+
+klutz = ClumsyPlayer.new("klutz", 105)
+knuckleheads.add_player(klutz)
+
 puts knuckleheads.title
 
 loop do
