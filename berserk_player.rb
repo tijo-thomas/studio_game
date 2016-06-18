8e63b137a70f2e07dfc682f55c64ec948626d321
Tijo <emailtijothomas@gmail.com>
2016-06-18 16:44:59 -0500

    Adds woot and blam methods that defaults to the 'Player' class methods if conditions aren't met.

--- berserk_player.rb
+++ berserk_player.rb
@@ -2,9 +2,34 @@ require_relative "player"
 
 class BerserkPlayer < Player
 	
-	def method_name
-		
+	def initialize(name, health=100)
+		super(name, health)
+		@woot_count = 0
 	end
 
+	def berserk?
+		@woot_count > 5
+	end
 
+	def woot
+		super
+		@woot_count += 1
+		puts "#{@name} is berserk!" if berserk?
+	end
+
+	def blam
+		if berserk?
+			woot
+		else
+			super
+		end
+	end
+
+end
+
+if __FILE__ == $0
+  berserker = BerserkPlayer.new("berserker", 50)
+  6.times { berserker.woot }
+  2.times { berserker.blam }
+  puts berserker.health
 end
